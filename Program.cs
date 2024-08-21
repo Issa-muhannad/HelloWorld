c5a2636bd03d24a85a5f23fec3879d48b6f71626
Muhannad <dbaxpress@gmail.com>
2024-08-21 17:27:19 -0400

    working with strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
+
+string firstFriend = "      maria      ";
+string secondFriend = "       sage     ";
+
+firstFriend = firstFriend.TrimStart(); //to remove space.
+
+
+Console.WriteLine($"My Friends are {firstFriend} and {secondFriend.Trim()}");
+
+// $ is called string interpolation. to concatenate a string with values of other strings, instead of using the + sign.
