6a3167dd8945bb1f77f8e85328443e6e77c12a2a
Muhannad <dbaxpress@gmail.com>
2024-08-24 00:14:02 -0400

    tried to change a commit message

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4fcea9576bbf97a56d25fca1fd071e1c859f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caee1d195166f5ead557c7b4d5aa458ed1180dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
