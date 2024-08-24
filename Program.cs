7d8a6d45d0db260dacee5ff0fcd661567c763729
Muhannad <dbaxpress@gmail.com>
2024-08-23 23:51:21 -0400

    numbers part 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 
 /*Console.WriteLine("Hello, World!");
 
@@ -25,12 +27,27 @@ Console.WriteLine(sayHello.ToLower());*/
 
 //search strings
 
-string songLyrics = "You say goodbye, and I say hello";
+/*string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.Contains("goodbye"));
 Console.WriteLine(songLyrics.Contains("hello"));
 Console.WriteLine(songLyrics.Contains("Hello"));
 //upper case is different from lower case. 
 Console.WriteLine(songLyrics.StartsWith("You"));
-Console.WriteLine(songLyrics.EndsWith("say"));
+Console.WriteLine(songLyrics.EndsWith("say"));*/
 
 
+
+
+// Numbers, Integers and math
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c); // shortcut conso + tab, wl + tab
+
+int y = 2100000000;
+int z = 2100000000;
+//long k = (long)y + (long)z;
+long k = checked(y + z); // this way we ask the compiler to tell us if there is an overflow and give us an error. runtime exception.
+//Unhandled exception. System.OverflowException: Arithmetic operation resulted in an overflow.
+Console.WriteLine(k);
