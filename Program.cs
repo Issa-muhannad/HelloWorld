642dea913823edeaa0bd75afb6f4093be667ef78
Muhannad <dbaxpress@gmail.com>
2024-08-27 00:01:18 -0400

    numbers section completed

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ Console.WriteLine(songLyrics.EndsWith("say"));*/
 
 // Numbers, Integers and math
 
-int a = 18;
+/*int a = 18;
 int b = 6;
 int c = a + b;
 Console.WriteLine(c); // shortcut conso + tab, wl + tab
@@ -50,4 +50,73 @@ int z = 2100000000;
 //long k = (long)y + (long)z;
 long k = checked(y + z); // this way we ask the compiler to tell us if there is an overflow and give us an error. runtime exception.
 //Unhandled exception. System.OverflowException: Arithmetic operation resulted in an overflow.
-Console.WriteLine(k);
+Console.WriteLine(k);*/
+
+// order of operations
+
+/*int a = 5;
+int b = 4;
+int c = 2;
+//int d = a + b * c;
+int d = (a + b) * c;
+Console.WriteLine(d);*/
+
+/*int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c; //integet division gives whole numbers
+Console.WriteLine(d);*/
+
+/*int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");*/
+
+/*int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");*/
+
+// double type
+
+/*double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);*/
+
+/*double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);*/
+
+// decimal types
+
+/*decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);*/
+
+/*double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);*/
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caee1d195166f5ead557c7b4d5aa458ed1180dd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3167dd8945bb1f77f8e85328443e6e77c12a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
